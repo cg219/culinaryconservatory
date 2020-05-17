15773bcbeebb5a4a54cc5ff784b1e0167613f233
Clemente <dev@imkreative.com>
2020-05-17 16:23:47 -0400

    Update with Rollovers and Shadows

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-// $('a[href^="#"]').on('click', event => {
-//     event.preventDefault();
-
-//     let link = $(event.currentTarget);
-//     let section = $(link.attr('href'));
-
-//     $('html, body').animate({
-//         scrollTop: section.offset().top
-//     }, 900);
-// });
-
-// const onCardClick = event => {
-//     $(event.currentTarget).toggleClass('showbio');
-// }
-
-// const presentSite = () => {
-//     $("body").addClass('present');
-// }
-
-// $('.card').on('click', onCardClick);
-
-// setTimeout(presentSite, 1000);
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
