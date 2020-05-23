e6245f9b43a9f358049afffc0be0aad429458bc5
Clemente <dev@imkreative.com>
2020-05-23 12:59:53 -0400

    COntact Form Stuff

--- src/components/contact-form/contact-form.component.jsx
+++ src/components/contact-form/contact-form.component.jsx
@@ -7,6 +7,7 @@ import "./contact-form.style.scss";
 // City entry.1669405955
 // State entry.1155721484
 // Phone entry.2108076832
+// https://docs.google.com/forms/d/e/1FAIpQLScYJSQomGuAgxgcmCU1kWyNv0Z1pD1JfFFjp8dd5jfqCXIk3A/formResponse
 
 class ContactForm extends React.Component {
     constructor(props) {
@@ -19,7 +20,39 @@ class ContactForm extends React.Component {
 
     sendForm = event => {
         event.preventDefault();
-        console.log(event);
+
+        let data = {
+            'entry.2127806314': 'Name',
+            'entry.399317233': 'Email',
+            'entry.10262297': 'Body',
+            'entry.1669405955': 'City',
+            'entry.1155721484': 'State',
+            'entry.2108076832': 'Phone'
+        };
+
+        let fm = new FormData();
+
+        fm.append('entry.2127806314', 'Name');
+        fm.append('entry.399317233', 'Email');
+        fm.append('entry.10262297', 'Body');
+        fm.append('entry.1669405955', 'City');
+        fm.append('entry.1155721484', 'State');
+        fm.append('entry.2108076832', 'Phone');
+
+        let options = {
+            method: 'POST',
+            body: fm,
+            responseType: 'json',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        fetch('https://docs.google.com/forms/d/e/1FAIpQLScYJSQomGuAgxgcmCU1kWyNv0Z1pD1JfFFjp8dd5jfqCXIk3A/formResponse', options)
+            .then(response => response.xml())
+            .then(data => console.log(`Success: ${data}`))
+            .catch(error => console.log(`Error: ${error}`))
     }
 
     render() {
@@ -28,26 +61,26 @@ class ContactForm extends React.Component {
                 <h3 className="contact-form-title">Contact Us</h3>
                 <form onSubmit={ this.sendForm }>
                     <div className="input-wrapper">
-                        <input type="text" placeholder="Name" />
+                        <input type="text" name="entry.2127806314" placeholder="Name" />
                     </div>
                     <div className="input-group">
                         <div className="input-wrapper">
-                            <input type="text" placeholder="Email" />
+                            <input type="text" name="entry.399317233" placeholder="Email" />
                         </div>
                         <div className="input-wrapper">
-                            <input type="text" placeholder="Phone" />
+                            <input type="text" name="entry.2108076832" placeholder="Phone" />
                         </div>
                     </div>
                     <div className="input-group">
                         <div className="input-wrapper">
-                            <input type="text" placeholder="City" />
+                            <input type="text" name="entry.1669405955" placeholder="City" />
                         </div>
                         <div className="input-wrapper">
-                            <input type="text" placeholder="State" />
+                            <input type="text" name="entry.1155721484" placeholder="State" />
                         </div>
                     </div>
                     <div className="input-wrapper">
-                        <textarea placeholder="Message"></textarea>
+                        <textarea name="entry.10262297" placeholder="Message"></textarea>
                     </div>
                     <input className="input-submit" value="Submit" type="submit" />
                 </form>
